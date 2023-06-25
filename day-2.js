dceeaaa927ef4af7de39b12b010392aed68029f1
Gabriel07-biel <bielgs9870@gmail.com>
2023-06-25 15:06:55 -0300

    Add files via upload

---
+++ day-2.js
@@ -0,0 +1,5 @@
+const nome = prompt("Qual seu nome?:");
+const idade = prompt("Qual a sua idade?:");
+const linguagem = prompt("Qual linguagem de programação você está aprendendo?:");
+const msg = `Olá ${nome}!, você possui ${idade} anos e já está aprendendo ${linguagem}`;
+alert(msg);
